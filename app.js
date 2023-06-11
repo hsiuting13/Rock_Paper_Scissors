931d331e54ea8c51f95addec661f5eb349d9a66a
Hsiu Ting <mr.hsiuting@gmail.com>
2023-06-11 15:27:37 -0700

    Fixes the logic of playRound

--- app.js
+++ app.js
@@ -4,19 +4,31 @@ function getComputerChoice() {
   let random_number = Math.floor(Math.random() * choices.length);
   return choices[random_number].toUpperCase();
 }
-
+let player_score = 0;
+let computer_score = 0;
 function playRound(playerSelection, computerSelection) {
-  //   let player = playerSelection.toUpperCase();
   if (playerSelection === computerSelection) {
     return "Tie";
   } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+    computer_score++;
     return "You Lose! Paper beats Rock";
   } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+    computer_score++;
     return "You lose! Rock beats Scissors";
+  } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+    computer_score++;
+    return "You lose! Scissors beats Paper";
   } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+    player_score++;
     return "You win! Rock beats Scissors";
+  } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+    player_score++;
+    return "You win! Paper beats Rock";
+  } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+    player_score++;
+    return "You win! Scissors beats Paper";
   } else {
-    return "You win! Rock beats Scissors";
+    return "Plz! Provide the correct information";
   }
 }
 const playerSelection = prompt(
@@ -24,3 +36,17 @@ const playerSelection = prompt(
 ).toUpperCase();
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+
+console.log(playRound(playerSelection, computerSelection));
+// function game() {
+//   const playerSelection = prompt(
+//     "Pick a choice: Rock, Paper, Scissors!"
+//   ).toUpperCase();
+//   const computerSelection = getComputerChoice();
+//   console.log(playRound(playerSelection, computerSelection));
+//   console.log(playRound(playerSelection, computerSelection));
+//   console.log(playRound(playerSelection, computerSelection));
+//   console.log(playRound(playerSelection, computerSelection));
+//   console.log(playRound(playerSelection, computerSelection));
+// }
+// game();
