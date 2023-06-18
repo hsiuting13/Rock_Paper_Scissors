52ef4cbe30c2d250c1d4404fa79a28ed8e75a23a
hsiu <mr.hsiuting@gmail.com>
2023-06-17 19:54:52 -0700

    Fixes the bug on  game()

--- app.js
+++ app.js
@@ -12,55 +12,50 @@ const scores = document.querySelector('.scores');
 let computer_score = 0;
 let player_score = 0;
 function playRound(playerSelection, computerSelection) {
- 
   if (playerSelection === computerSelection) {
     rounds.textContent = "Tie";
   } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
-    rounds.textContent = "You Lose! Paper beats Rock";
     computer_score++;
+    return rounds.textContent = "You Lose! Paper beats Rock";
+    
   } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
-   
-    rounds.textContent = "You lose! Rock beats Scissors";
     computer_score++;
+    return "You lose! Rock beats Scissors";
   } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-   
-    rounds.textContent = "You lose! Scissors beats Paper";
     computer_score++;
+    return "You lose! Scissors beats Paper";
   } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
-   
-    rounds.textContent = "You win! Rock beats Scissors";
     player_score++;
+    return "You win! Rock beats Scissors";
   } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
-    
-    rounds.textContent = "You win! Paper beats Rock";
     player_score++;
+    return "You win! Paper beats Rock";
   } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-    
-    rounds.textContent = "You win! Scissors beats Paper";
     player_score++;
+    return "You win! Scissors beats Paper";
   }
 }
 function game(){
+  // if(player_score === 5 || computer_score === 5){
+    
+  // }
   if(player_score === 5) {
-    result.textContent = `You win the game! `
-  } else if(computer_score === 5){
-    result.textContent = `You lose the game! `
-  }
-  if(player_score === 5 || computer_score === 5){
     player_score = 0;
     computer_score = 0;
+    return `You win the game!`;
+  } else if(computer_score === 5){
+    player_score = 0;
+    computer_score = 0;
+    return`You lose the game!`;
   }
+ 
 }
-
-
 
 btns.forEach((btn) => btn.addEventListener('click', () => {
   const playerSelection = btn.textContent.toUpperCase();
   const computerSelection = getComputerChoice();
-  
-  playRound(playerSelection, computerSelection);
+  rounds.textContent = playRound(playerSelection, computerSelection);
   scores.textContent = `Computer's score ${computer_score}  :  Playes's score ${player_score}`
-  // console.log(playerSelection)
-  // console.log(computerSelection)
-  game();
+  result.textContent = game();
+  
 }));
